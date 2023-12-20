8d1565bfda5eac9053321da960f3da73155ddcbf
João Henrique Lessa <joaohenriquerlessa@gmail..com>
2023-12-19 22:17:29 -0300

    cabei

---
+++ povoamento.sql
@@ -0,0 +1,263 @@
+--INSERINDO PESSOAS
+
+-- Instance 1
+INSERT INTO Pessoas(Nome, Endereço, CPF, Nascimento, CPF_Indica) VALUES ('João Pedro', 'Rua um 24', '244.244.244-23', '2002-02-24', '333.555.777-00');
+
+-- Instance 2
+INSERT INTO Pessoas(Nome, Endereço, CPF, Nascimento, CPF_Indica) VALUES ('Maria Silva', 'Avenida dois 36', '123.456.789-12', '1995-07-15', '244.244.244-23');
+
+-- Instance 3
+INSERT INTO Pessoas(Nome, Endereço, CPF, Nascimento, CPF_Indica) VALUES ('Carlos Oliveira', 'Praça três 48', '987.654.321-98', '1980-11-10', '123.456.789-12');
+
+-- Instance 4
+INSERT INTO Pessoas(Nome, Endereço, CPF, Nascimento, CPF_Indica) VALUES ('Ana Souza', 'Alameda quatro 12', '555.111.444-99', '1988-05-30', '987.654.321-98');
+
+-- Instance 5
+INSERT INTO Pessoas(Nome, Endereço, CPF, Nascimento, CPF_Indica) VALUES ('Lucas Santos', 'Estrada cinco 92', '777.888.999-11', '1999-09-18', '555.111.444-99');
+
+-- Instance 6
+INSERT INTO Pessoas(Nome, Endereço, CPF, Nascimento, CPF_Indica) VALUES ('Isabel Oliveira', 'Avenida seis 17', '111.222.333-44', '1985-03-12', '777.888.999-11');
+
+-- Instance 7
+INSERT INTO Pessoas(Nome, Endereço, CPF, Nascimento, CPF_Indica) VALUES ('Rodrigo Lima', 'Rua sete 59', '333.555.777-00', '1992-12-01', '111.222.333-44');
+
+-- Instance 8
+INSERT INTO Pessoas(Nome, Endereço, CPF, Nascimento, CPF_Indica) VALUES ('Mariana Costa', 'Avenida oito 23', '666.999.222-55', '1983-08-22', '333.555.777-00');
+
+-- Instance 9
+INSERT INTO Pessoas(Nome, Endereço, CPF, Nascimento, CPF_Indica) VALUES ('Pedro Rocha', 'Praça nove 78', '222.333.444-55', '1991-06-07', '666.999.222-55');
+
+-- Instance 10
+INSERT INTO Pessoas(Nome, Endereço, CPF, Nascimento, CPF_Indica) VALUES ('Fernanda Pereira', 'Rua dez 42', '888.777.666-22', '1987-04-15', '222.333.444-55');
+
+-- Instance 11
+INSERT INTO Pessoas(Nome, Endereço, CPF, Nascimento, CPF_Indica) VALUES ('Gabriel Santos', 'Avenida onze 88', '999.888.777-66', '1994-10-05', '888.777.666-22');
+
+-- Instance 12
+INSERT INTO Pessoas(Nome, Endereço, CPF, Nascimento, CPF_Indica) VALUES ('Amanda Lima', 'Rua doze 33', '444.333.222-11', '1997-07-20', '999.888.777-66');
+
+-- Instance 13
+INSERT INTO Pessoas(Nome, Endereço, CPF, Nascimento, CPF_Indica) VALUES ('Rafaela Oliveira', 'Praça treze 54', '111.222.333-44', '1986-04-25', '444.333.222-11');
+
+-- Instance 14
+INSERT INTO Pessoas(Nome, Endereço, CPF, Nascimento, CPF_Indica) VALUES ('Bruno Costa', 'Alameda quatorze 77', '555.444.333-22', '1990-02-15', '111.222.333-44');
+
+-- Instance 15
+INSERT INTO Pessoas(Nome, Endereço, CPF, Nascimento, CPF_Indica) VALUES ('Carolina Pereira', 'Estrada quinze 21', '777.666.555-44', '1998-11-08', '555.444.333-22');
+
+-- Instance 16
+INSERT INTO Pessoas(Nome, Endereço, CPF, Nascimento, CPF_Indica) VALUES ('Matheus Rocha', 'Rua dezesseis 96', '222.333.444-55', '1989-08-18', '777.666.555-44');
+
+-- Instance 17
+INSERT INTO Pessoas(Nome, Endereço, CPF, Nascimento, CPF_Indica) VALUES ('Isabella Lima', 'Avenida dezessete 12', '666.555.444-11', '1996-06-03', '222.333.444-55');
+
+-- Instance 18
+INSERT INTO Pessoas(Nome, Endereço, CPF, Nascimento, CPF_Indica) VALUES ('Henrique Costa', 'Praça dezoito 45', '333.222.111-99', '1984-03-30', '666.555.444-11');
+
+-- Instance 19
+INSERT INTO Pessoas(Nome, Endereço, CPF, Nascimento, CPF_Indica) VALUES ('Laura Santos', 'Alameda dezenove 78', '888.777.666-22', '1993-12-10', '333.222.111-99');
+
+-- Instance 20
+INSERT INTO Pessoas(Nome, Endereço, CPF, Nascimento, CPF_Indica) VALUES ('Felipe Oliveira', 'Rua vinte 54', '333.555.777-00', '1982-01-22', '888.777.666-22');
+
+
+--INSERINDO TELEFONES
+-- Example 1
+INSERT INTO Telefones(CPF, Telefone) VALUES ('244.244.244-23', '8199494-1888');
+
+-- Example 2
+INSERT INTO Telefones(CPF, Telefone) VALUES ('123.456.789-12', '8199456-7890');
+
+-- Example 3
+INSERT INTO Telefones(CPF, Telefone) VALUES ('987.654.321-98', '8798776-5432');
+
+-- Example 4
+INSERT INTO Telefones(CPF, Telefone) VALUES ('555.111.444-99', '2198877-7665');
+
+-- Example 5
+INSERT INTO Telefones(CPF, Telefone) VALUES ('777.888.999-11', '8399966-3322');
+
+-- Example 6
+INSERT INTO Telefones(CPF, Telefone) VALUES ('111.222.333-44', '8199812-3456');
+
+-- Example 7
+INSERT INTO Telefones(CPF, Telefone) VALUES ('333.555.777-00', '2198745-6321');
+
+-- Example 8
+INSERT INTO Telefones(CPF, Telefone) VALUES ('666.999.222-55', '8998899-2223');
+
+-- Example 9
+INSERT INTO Telefones(CPF, Telefone) VALUES ('222.333.444-55', '8798776-5543');
+
+-- Example 10
+INSERT INTO Telefones(CPF, Telefone) VALUES ('888.777.666-22', '8198234-5678');
+
+-- Example 11
+INSERT INTO Telefones(CPF, Telefone) VALUES ('999.888.777-66', '8998987-6543');
+
+-- Example 12
+INSERT INTO Telefones(CPF, Telefone) VALUES ('444.333.222-11', '8798776-6655');
+
+-- Example 13
+INSERT INTO Telefones(CPF, Telefone) VALUES ('777.666.555-44', '2198111-2223');
+
+-- Example 14
+INSERT INTO Telefones(CPF, Telefone) VALUES ('222.333.444-55', '8398345-6789');
+
+-- Example 15
+INSERT INTO Telefones(CPF, Telefone) VALUES ('666.555.444-11', '8798765-4433');
+
+-- Example 16
+INSERT INTO Telefones(CPF, Telefone) VALUES ('333.222.111-99', '2198442-2233');
+
+-- Example 17
+INSERT INTO Telefones(CPF, Telefone) VALUES ('888.777.666-22', '8899966-7555');
+
+-- Example 18
+INSERT INTO Telefones(CPF, Telefone) VALUES ('333.555.777-00', '8198345-6789');
+
+-- Example 19
+INSERT INTO Telefones(CPF, Telefone) VALUES ('777.666.555-44', '2199888-9876');
+
+-- Example 20
+INSERT INTO Telefones(CPF, Telefone) VALUES ('333.555.777-00', '8798776-6655');
+
+--INSERINDO PESSOAS MOVIMENTAM CONTAS
+-- Example 1
+INSERT INTO Pessoas_movimentam_contas(CPF, ID_Conta, Valor, DataHora) VALUES ('244.244.244-23', 1,  90.00, 187654321);
+
+-- Example 2
+INSERT INTO Pessoas_movimentam_contas(CPF, ID_Conta, Valor, DataHora) VALUES ('123.456.789-12', 2,  764.00, 987654322);
+
+-- Example 3
+INSERT INTO Pessoas_movimentam_contas(CPF, ID_Conta, Valor, DataHora) VALUES ('987.654.321-98', 3,  2301.00, 345678912);
+
+-- Example 4
+INSERT INTO Pessoas_movimentam_contas(CPF, ID_Conta, Valor, DataHora) VALUES ('555.111.444-99', 4,  88.00, 678912345);
+
+-- Example 5
+INSERT INTO Pessoas_movimentam_contas(CPF, ID_Conta, Valor, DataHora) VALUES ('777.888.999-11', 5,  456.00, 123456789);
+
+-- Example 6
+INSERT INTO Pessoas_movimentam_contas(CPF, ID_Conta, Valor, DataHora) VALUES ('111.222.333-44', 6,  7843.00, 234567890);
+
+-- Example 7
+INSERT INTO Pessoas_movimentam_contas(CPF, ID_Conta, Valor, DataHora) VALUES ('333.555.777-00', 7,  92.00, 456789123);
+
+-- Example 8
+INSERT INTO Pessoas_movimentam_contas(CPF, ID_Conta, Valor, DataHora) VALUES ('666.999.222-55', 8,  120.00, 567891234);
+
+-- Example 9
+INSERT INTO Pessoas_movimentam_contas(CPF, ID_Conta, Valor, DataHora) VALUES ('222.333.444-55', 9,  432.00, 789123456);
+
+-- Example 10
+INSERT INTO Pessoas_movimentam_contas(CPF, ID_Conta, Valor, DataHora) VALUES ('888.777.666-22', 10,  7645.00, 891234567);
+
+-- Example 11
+INSERT INTO Pessoas_movimentam_contas(CPF, ID_Conta, Valor, DataHora) VALUES ('999.888.777-66', 11,  123.00, 234567891);
+
+-- Example 12
+INSERT INTO Pessoas_movimentam_contas(CPF, ID_Conta, Valor, DataHora) VALUES ('444.333.222-11', 12,  678.00, 456789123);
+
+-- Example 13
+INSERT INTO Pessoas_movimentam_contas(CPF, ID_Conta, Valor, DataHora) VALUES ('777.666.555-44', 13,  94.00, 567891234);
+
+-- Example 14
+INSERT INTO Pessoas_movimentam_contas(CPF, ID_Conta, Valor, DataHora) VALUES ('222.333.444-55', 14,  5678.00, 789123456);
+
+-- Example 15
+INSERT INTO Pessoas_movimentam_contas(CPF, ID_Conta, Valor, DataHora) VALUES ('666.555.444-11', 15,  983.00, 891234567);
+
+-- Example 16
+INSERT INTO Pessoas_movimentam_contas(CPF, ID_Conta, Valor, DataHora) VALUES ('333.222.111-99', 16,  1234.00, 345678912);
+
+-- Example 17
+INSERT INTO Pessoas_movimentam_contas(CPF, ID_Conta, Valor, DataHora) VALUES ('888.777.666-22', 17,  567.00, 456789123);
+
+-- Example 18
+INSERT INTO Pessoas_movimentam_contas(CPF, ID_Conta, Valor, DataHora) VALUES ('333.555.777-00', 18,  76.00, 567891234);
+
+-- Example 19
+INSERT INTO Pessoas_movimentam_contas(CPF, ID_Conta, Valor, DataHora) VALUES ('777.666.555-44', 19,  4567.00, 789123456);
+
+-- Example 20
+INSERT INTO Pessoas_movimentam_contas(CPF, ID_Conta, Valor, DataHora) VALUES ('333.555.777-00', 20,  89.00, 891234567);
+
+
+INSERT INTO Conta (Saldo) VALUES (1000.00), (200.00), (500.00), 
+(70.00), (900.00), (200.00), (500.00), (70.00), (15.00), 
+(200.00), (500.00), (70.00), (35.00), (200.00), (500.00), 
+(70.00),(700.00), (200.00), (70.00), (70.00);
+
+INSERT INTO Dados_Bancarios (Conta, Agencia) VALUES (1, "0001-2", 1),
+(2, "0002-3", 2), (3, "0004-2", 3), (4, "0003-3"), (5, "0405-2"), 
+(6, "4001-3"), (7, "4002-2"), (8, "4001-3"), (9, "4001-2"), 
+(10, "4001-3"), (11, "4504-2"), (12, "2101-3"), (13, "4500-2"), 
+(14, "4002-3"), (15, "0002-2"), (16, "0404-3"), (17, "0402-2"), 
+(18, "4004-3"), (19, "4005-2"), (20, "0105-3");
+
+INSERT INTO Evento_Esportivo (Mandante, Visitante, Local) VALUES 
+("São Paulo", "Corinthians", "Morumbi", 1), ("Palmeiras", "Santos", "Allianz Parque", 2), 
+("Flamengo", "Fluminense", "Maracanã", 3), ("Vasco", "Botafogo", "São Januário", 4), 
+("Grêmio", "Internacional", "Arena do Grêmio", 5), ("Atlético-MG", "Cruzeiro", "Mineirão", 6), 
+("Bahia", "Vitória", "Fonte Nova", 7), ("Sport", "Náutico", "Ilha do Retiro", 8), 
+("Ceará", "Fortaleza", "Castelão", 9), ("Coritiba", "Atlético-PR", "Couto Pereira", 10), 
+("Ponte Preta", "Guarani", "Moisés Lucarelli", 11), ("São Caetano", "Santo André", "Anacleto Campanella", 12), 
+("São Bento", "São Bernardo", "Walter Ribeiro", 13), ("Juventus", "Portuguesa", "Conde Rodolfo Crespi", 14), 
+("Santos", "São Paulo", "Vila Belmiro", 15), ("Corinthians", "Palmeiras", "Arena Corinthians", 16), 
+("Fluminense", "Vasco", "Maracanã", 17), ("Botafogo", "Flamengo", "Engenhão", 18), 
+("Internacional", "Grêmio", "Beira-Rio", 19), ("Cruzeiro", "Atlético-MG", "Mineirão", 20);
+
+INSERT INTO DataHora (ID_Evento, DataHora) VALUES (1, 73453453), 
+(2, 453783453), (3, 453453453), (4, 453453783), 
+(5, 4534537834), (6, 4534345345), (7, 4534213453), 
+(8, 453453), (9, 45345345), (10, 4534532), 
+(11, 7827821), (12, 87637243), (13, 53412345), 
+(14, 7427243), (15, 4532402), (16, 74845234531), 
+(17, 45345345), (18, 4534531235), (19, 453453453), 
+(20, 4537453);
+
+INSERT INTO Bonus (CPF, Codigo_Bonus, Valor) VALUES ('244.244.244-23', 'BONUS10', 35.00),
+('123.456.789-12', 'BONUS20', 45.00), ('987.654.321-98', 'BONUS30', 245.00), 
+('555.111.444-99', 'BONUS40', 40.00), ('777.888.999-11', 'BONUS50', 45.00), 
+('111.222.333-44', 'BONUS60', 54.00), ('333.555.777-00', 'BONUS70', 25.00), 
+('666.999.222-55', 'BONUS80', 200.00), ('222.333.444-55', 'BONUS90', 45.00), 
+('888.777.666-22', 'BONUS100', 24.00), ('444.333.222-11', 'BONUS110', 27.00), 
+('111.222.333-44', 'BONUS120', 14.00), ('555.444.333-22', 'BONUS130', 45.00), 
+('777.666.555-44', 'BONUS140', 28.00), ('222.333.444-55', 'BONUS150', 24.00), 
+('666.555.444-11', 'BONUS160', 27.00), ('333.222.111-99', 'BONUS170', 45.00), 
+('888.777.666-22', 'BONUS180', 21.00), ('333.555.777-00', 'BONUS190', 245.00), 
+('333.555.777-00', 'BONUS200', 24.00);
+
+INSERT INTO Gols (ID_Aposta, Quantidade) VALUES (1, 4), (2, 2), (3, 1), 
+(4, 3), (5, 2), (6, 1), (7,0), (8, 1), (9, 2), (10, 3), (11, 2), (12, 1), 
+(13, 0), (14, 1), (15, 2), (16, 3), (17, 2), (18, 1), (19, 0), (20, 1);
+
+INSERT INTO Placar_Exato(ID_Aposta, Gol_Mandante, Gol_Visitante) VALUES (1, 3, 1), 
+(2, 1, 1), (3, 1, 0), (4, 2, 1), (5, 1, 1), (6, 1, 0), (7, 0, 0), (8, 1, 0), 
+(9, 1, 1), (10, 2, 1), (11, 1, 1), (12, 1, 0), (13, 0, 0), (14, 1, 0), 
+(15, 1, 1), (16, 2, 1), (17, 1, 1), (18, 1, 0), (19, 0, 0), (20, 1, 0);
+
+INSERT INTO Resultado(ID_Aposta, Resultado) VALUES (1, 'Mandante'), 
+(2, 'Empate'), (3, 'Mandante'), (4, 'Mandante'), (5, 'Empate'), 
+(6, 'Mandante'), (7, 'Empate'), (8, 'Mandante'), (9, 'Empate'), 
+(10, 'Mandante'), (11, 'Empate'), (12, 'Mandante'), (13, 'Empate'), 
+(14, 'Mandante'), (15, 'Empate'), (16, 'Mandante'), (17, 'Empate'), 
+(18, 'Mandante'), (19, 'Empate'), (20, 'Mandante');
+
+INSERT INTO Ambos_Marcam(ID_Aposta, Sim/Nao) VALUES (1, true), (2, true), 
+(3, false), (4, true), (5, true), (6, false), (7, false), (8, false), 
+(9, true), (10, true), (11, true), (12, false), (13, false), (14, false), 
+(15, true), (16, true), (17, true), (18, false), (19, false), (20, false);
+
+INSERT INTO Apostar(CPF, ID_Conta, ID_Evento, ID_Aposta, Valor)	VALUES ('244.244.244-23', 1, 1, 1, 10.00), 
+('123.456.789-12', 2, 2, 2, 20.00), ('987.654.321-98', 3, 3, 3, 30.00), 
+('555.111.444-99', 4, 4, 4, 40.00), ('777.888.999-11', 5, 5, 5, 50.00), 
+('111.222.333-44', 6, 6, 6, 60.00), ('333.555.777-00', 7, 7, 7, 70.00), 
+('666.999.222-55', 8, 8, 8, 80.00), ('222.333.444-55', 9, 9, 9, 90.00), 
+('888.777.666-22', 10, 10, 10, 100.00), ('444.333.222-11', 11, 11, 11, 110.00), 
+('111.222.333-44', 12, 12, 12, 120.00), ('555.444.333-22', 13, 13, 13, 130.00), 
+('777.666.555-44', 14, 14, 14, 140.00), ('222.333.444-55', 15, 15, 15, 150.00), 
+('666.555.444-11', 16, 16, 16, 160.00), ('333.222.111-99', 17, 17, 17, 170.00), 
+('888.777.666-22', 18, 18, 18, 180.00), ('333.555.777-00', 19, 19, 19, 190.00), 
+('333.555.777-00', 20, 20, 20, 200.00);
