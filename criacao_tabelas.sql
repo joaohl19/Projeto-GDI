293962eadb6c7edc15f5d5684721fb0206e481c7
damattag <guilherme.damatta@yahoo.com.br>
2023-12-19 21:48:48 -0300

    added check in result table

--- criacao_tabelas.sql
+++ criacao_tabelas.sql
@@ -99,13 +99,13 @@ CREATE TABLE Placar_Exato (
 
 CREATE TABLE Resultado (
     ID_Aposta NUMBER,
-    Resultado VARCHAR2(255),
+    Resultado VARCHAR2(255) CHECK (Resultado IN ('Mandante', 'Visitante', 'Empate'))
     CONSTRAINT resultado_fk FOREIGN KEY (ID_Aposta) REFERENCES Aposta(ID_Aposta)
 );
 
 CREATE TABLE Ambos_Marcam (
     ID_Aposta NUMBER,
-    Sim_Nao VARCHAR2(3) CHECK (Sim_Nao IN ('Sim', 'Nao'))
+    Sim_Nao BIT NOT NULL,
     CONSTRAINT ambos_marcam_fk FOREIGN KEY (ID_Aposta) REFERENCES Aposta(ID_Aposta)
 );
 
