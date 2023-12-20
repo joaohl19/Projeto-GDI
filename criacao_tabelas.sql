6d972527fb67469bd70d4f1700f5b9055df20bf9
Renato Serrano <renatom.sandrade@gmail.com>
2023-12-19 21:08:40 -0300

    refactor constraints

--- criacao_tabelas.sql
+++ criacao_tabelas.sql
@@ -14,103 +14,106 @@ DROP TABLE Ambos_Marcam;
 DROP TABLE Apostar;
 
 CREATE TABLE Pessoas (
-    Nome VARCHAR(255),
-    Endereço VARCHAR(255),
-    CPF VARCHAR(20) PRIMARY KEY,
+    Nome VARCHAR2(255),
+    Endereço VARCHAR2(255),
+    CPF VARCHAR2(20),
     Nascimento DATE,
-    CPF_Indica VARCHAR(20),
-    FOREIGN KEY (CPF_Indica) REFERENCES Pessoas(CPF)
+    CPF_Indica VARCHAR2(20),
+    CONSTRAINT pessoas_pk PRIMARY KEY (CPF),
+    CONSTRAINT pessoas_fk FOREIGN KEY (CPF_Indica) REFERENCES Pessoas(CPF)
 );
 
-
 CREATE TABLE Telefones (
-    CPF VARCHAR(20),
-    Telefone VARCHAR(20),
-    FOREIGN KEY (CPF) REFERENCES Pessoas(CPF)
+    CPF VARCHAR2(20),
+    Telefone VARCHAR2(20),
+    CONSTRAINT telefones_fk FOREIGN KEY (CPF) REFERENCES Pessoas(CPF)
 );
 
 CREATE TABLE Conta (
-    ID_Conta INT DEFAULT seq_conta.NEXTVAL PRIMARY KEY,
-    Saldo DECIMAL(10,2)
+    ID_Conta NUMBER DEFAULT seq_conta.NEXTVAL,
+    Saldo DECIMAL(10,2),
+    CONSTRAINT conta_pk PRIMARY KEY (ID_Conta)
 );
 
 CREATE TABLE Dados_Bancarios (
-    Conta VARCHAR(20),
-    Agencia VARCHAR(20),
-    ID_Conta INT,
-    FOREIGN KEY (ID_Conta) REFERENCES Conta(ID_Conta)
+    Conta VARCHAR2(20),
+    Agencia VARCHAR2(20),
+    ID_Conta NUMBER,
+    CONSTRAINT dados_bancarios_fk FOREIGN KEY (ID_Conta) REFERENCES Conta(ID_Conta)
 );
 
 CREATE TABLE Pessoas_movimentam_contas (
-    CPF VARCHAR(20),
-    ID_Conta INT,
+    CPF VARCHAR2(20),
+    ID_Conta NUMBER,
     Valor DECIMAL(10,2),
     DataHora TIMESTAMP,
-    FOREIGN KEY (CPF) REFERENCES Pessoas(CPF),
-    FOREIGN KEY (ID_Conta) REFERENCES Conta(ID_Conta),
-    PRIMARY KEY (CPF, ID_Conta)
+    CONSTRAINT pmc_pk PRIMARY KEY (CPF, ID_Conta),
+    CONSTRAINT pmc_fk_cpf FOREIGN KEY (CPF) REFERENCES Pessoas(CPF),
+    CONSTRAINT pmc_fk_id_conta FOREIGN KEY (ID_Conta) REFERENCES Conta(ID_Conta)
 );
 
 CREATE TABLE Evento_Esportivo (
-    Mandante VARCHAR(255),
-    Visitante VARCHAR(255),
-    Local VARCHAR(255),
-    ID_Evento INT DEFAULT seq_evento.NEXTVAL PRIMARY KEY
+    Mandante VARCHAR2(255),
+    Visitante VARCHAR2(255),
+    Local VARCHAR2(255),
+    ID_Evento NUMBER DEFAULT seq_evento.NEXTVAL,
+    CONSTRAINT evento_esportivo_pk PRIMARY KEY (ID_Evento)
 );
 
 CREATE TABLE DataHora (
     DataHora TIMESTAMP,
-    ID_Evento INT,
-    FOREIGN KEY (ID_Evento) REFERENCES Evento_Esportivo(ID_Evento)
+    ID_Evento NUMBER,
+    CONSTRAINT datahora_fk FOREIGN KEY (ID_Evento) REFERENCES Evento_Esportivo(ID_Evento)
 );
 
 CREATE TABLE Aposta (
-    ID_Aposta INT DEFAULT seq_aposta.NEXTVAL PRIMARY KEY
+    ID_Aposta NUMBER DEFAULT seq_aposta.NEXTVAL,
+    CONSTRAINT aposta_pk PRIMARY KEY (ID_Aposta)
 );
 
 CREATE TABLE Bonus (
-    CPF VARCHAR(20),
-    Codigo_Bonus VARCHAR(20),
+    CPF VARCHAR2(20),
+    Codigo_Bonus VARCHAR2(20),
     Valor DECIMAL(10,2),
-    FOREIGN KEY (CPF) REFERENCES Pessoas(CPF)
+    CONSTRAINT bonus_fk FOREIGN KEY (CPF) REFERENCES Pessoas(CPF)
 );
 
 CREATE TABLE Gols (
-    ID_Aposta INT,
+    ID_Aposta NUMBER,
     Quantidade INT,
-    FOREIGN KEY (ID_Aposta) REFERENCES Aposta(ID_Aposta)
+    CONSTRAINT gols_fk FOREIGN KEY (ID_Aposta) REFERENCES Aposta(ID_Aposta)
 );
 
 CREATE TABLE Placar_Exato (
-    ID_Aposta INT,
+    ID_Aposta NUMBER,
     Gol_Mandante INT,
     Gol_Visitante INT,
-    FOREIGN KEY (ID_Aposta) REFERENCES Aposta(ID_Aposta)
+    CONSTRAINT placar_exato_fk FOREIGN KEY (ID_Aposta) REFERENCES Aposta(ID_Aposta)
 );
 
 CREATE TABLE Resultado (
-    ID_Aposta INT,
-    Resultado VARCHAR(255),
-    FOREIGN KEY (ID_Aposta) REFERENCES Aposta(ID_Aposta)
+    ID_Aposta NUMBER,
+    Resultado VARCHAR2(255),
+    CONSTRAINT resultado_fk FOREIGN KEY (ID_Aposta) REFERENCES Aposta(ID_Aposta)
 );
 
 CREATE TABLE Ambos_Marcam (
-    ID_Aposta INT,
-    Sim_Nao VARCHAR(3),
-    FOREIGN KEY (ID_Aposta) REFERENCES Aposta(ID_Aposta)
+    ID_Aposta NUMBER,
+    Sim_Nao VARCHAR2(3),
+    CONSTRAINT ambos_marcam_fk FOREIGN KEY (ID_Aposta) REFERENCES Aposta(ID_Aposta)
 );
 
 CREATE TABLE Apostar (
-    CPF VARCHAR(20),
-    ID_Conta INT,
-    ID_Evento INT,
-    ID_Aposta INT,
+    CPF VARCHAR2(20),
+    ID_Conta NUMBER,
+    ID_Evento NUMBER,
+    ID_Aposta NUMBER,
     Valor DECIMAL(10,2),
-    FOREIGN KEY (CPF) REFERENCES Pessoas(CPF),
-    FOREIGN KEY (ID_Conta) REFERENCES Conta(ID_Conta),
-    FOREIGN KEY (ID_Evento) REFERENCES Evento_Esportivo(ID_Evento),
-    FOREIGN KEY (ID_Aposta) REFERENCES Aposta(ID_Aposta),
-    PRIMARY KEY (CPF, ID_Conta, ID_Evento, ID_Aposta)
+    CONSTRAINT apostar_pk PRIMARY KEY (CPF, ID_Conta, ID_Evento, ID_Aposta),
+    CONSTRAINT apostar_fk_cpf FOREIGN KEY (CPF) REFERENCES Pessoas(CPF),
+    CONSTRAINT apostar_fk_id_conta FOREIGN KEY (ID_Conta) REFERENCES Conta(ID_Conta),
+    CONSTRAINT apostar_fk_id_evento FOREIGN KEY (ID_Evento) REFERENCES Evento_Esportivo(ID_Evento),
+    CONSTRAINT apostar_fk_id_aposta FOREIGN KEY (ID_Aposta) REFERENCES Aposta(ID_Aposta)
 );
 
 CREATE SEQUENCE seq_conta
