5270922ab36fb9691e99ee18b656ec6a80cf56ba
João Pedro <5117.joaopedro@gmail.com>
2023-12-22 20:05:35 -0300

    triggers

--- consultas_pl.sql
+++ consultas_pl.sql
@@ -17,5 +17,35 @@
 --CREATE OR REPLACE PACKAGE
 --CREATE OR REPLACE PACKAGE BODY
 --CREATE OR REPLACE TRIGGER (COMANDO)
---CREATE OR REPLACE TRIGGER (LINHA)
+-- Trigger para impedir que uma pessoa seja excluída se ela possuir apostas associadas
+CREATE OR REPLACE TRIGGER BEFORE_DELETE_PESSOAS
+BEFORE DELETE ON Pessoas
+FOR EACH ROW
+DECLARE
+    contador_apostas NUMBER;
+BEGIN
+    -- Verifica se a pessoa possui apostas
+    SELECT COUNT(*)
+    INTO contador_apostas
+    FROM Apostar
+    WHERE CPF = :OLD.CPF;
 
+    -- Se houver apostas, impede a exclusão
+    IF contador_apostas > 0 THEN
+        RAISE_APPLICATION_ERROR(-20001, 'Não é possível excluir uma pessoa com apostas.');
+    END IF;
+END;
+/
+
+--CREATE OR REPLACE TRIGGER (LINHA)
+-- Trigger para impedir que seja inserida uma conta com saldo negativo
+CREATE OR REPLACE TRIGGER VerificarSaldoNegativo
+BEFORE INSERT ON Conta
+FOR EACH ROW
+DECLARE
+BEGIN
+    IF :NEW.Saldo < 0 THEN
+        RAISE_APPLICATION_ERROR(-20001, 'Não é permitido inserir uma conta com saldo negativo.');
+    END IF;
+END;
+/
