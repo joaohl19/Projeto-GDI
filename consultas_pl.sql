1ce4f938f3084ab721441910f039f4e33687ffea
João Pedro <5117.joaopedro@gmail.com>
2023-12-22 22:16:28 -0300

    PLs

--- consultas_pl.sql
+++ consultas_pl.sql
@@ -34,7 +34,7 @@ BEGIN
    DBMS_OUTPUT.PUT_LINE('Valor para Chave2: ' || tabela1('Chave2'));
 END;
 
---BLOCO ANÔNIMO
+--BLOCO ANÔNIMO + SELECT … INTO
 DECLARE
    ID_conta NUMBER;
    Saldo DECIMAL(10, 2);
@@ -68,12 +68,84 @@ EXEC CONSULTA_PARTIDAS('Fluminense', 'Vasco')
 --LOOP EXIT WHEN
 --WHILE LOOP
 --FOR IN LOOP
---SELECT … INTO
 --CURSOR (OPEN, FETCH e CLOSE)
 --EXCEPTION WHEN
---USO DE PAR METROS (IN, OUT ou IN OUT)
+
+--USO DE PARAMETROS (IN, OUT ou IN OUT)
+CREATE OR REPLACE PROCEDURE AtualizarSaldo (
+  p_id_conta IN Conta.ID_Conta%TYPE,
+  p_valor IN OUT DECIMAL
+) IS
+  v_saldo_atual DECIMAL(10,2);
+BEGIN
+  -- Obtém o saldo atual da conta
+  SELECT Saldo
+  INTO v_saldo_atual
+  FROM Conta
+  WHERE ID_Conta = p_id_conta;
+
+  -- Atualiza o saldo da conta
+  UPDATE Conta
+  SET Saldo = v_saldo_atual + p_valor
+  WHERE ID_Conta = p_id_conta;
+
+  -- Atualiza o valor de saída
+  p_valor := p_valor + v_saldo_atual;
+
+  -- Exibe o resultado
+  DBMS_OUTPUT.PUT_LINE('Saldo atualizado para: ' || (p_valor));
+END AtualizarSaldo;
+/
+
+-- Exemplo de uso do procedimento
+DECLARE
+  v_id_conta Conta.ID_Conta%TYPE := 1;
+  v_valor DECIMAL := 2000.00;
+BEGIN
+  AtualizarSaldo(v_id_conta, v_valor);
+END;
+/
+
 --CREATE OR REPLACE PACKAGE
+CREATE OR REPLACE PACKAGE PacoteExemplo AS
+    -- Declaração de Função Pública para Calcular Saldo Total
+    FUNCTION CalcularSaldoTotal(p_cpf IN VARCHAR2) RETURN DECIMAL;
+END PacoteExemplo;
+/
+
 --CREATE OR REPLACE PACKAGE BODY
+CREATE OR REPLACE PACKAGE BODY PacoteExemplo AS
+    -- Implementação da Função para Calcular Saldo Total
+    FUNCTION CalcularSaldoTotal(p_cpf IN VARCHAR2) RETURN DECIMAL IS
+        v_saldo_total DECIMAL(10,2) := 0;
+
+        -- Cursor para Obter Saldo da Conta
+        CURSOR c_saldo_conta (cpf_param VARCHAR2) IS
+            SELECT Saldo
+            FROM Conta c
+            JOIN Pessoas_movimentam_contas pmc ON c.ID_Conta = pmc.ID_Conta
+            WHERE pmc.CPF = cpf_param;
+
+    BEGIN
+        -- Loop pelo Cursor e Soma dos Saldos
+        FOR saldo_rec IN c_saldo_conta(p_cpf) LOOP
+            v_saldo_total := v_saldo_total + saldo_rec.Saldo;
+        END LOOP;
+
+        -- Retornar o Saldo Total
+        RETURN v_saldo_total;
+    END CalcularSaldoTotal;
+END PacoteExemplo;
+/
+
+--Exemplo de teste para o pacote
+DECLARE
+    v_saldo_total DECIMAL(10,2);
+BEGIN
+    v_saldo_total := PacoteExemplo.CalcularSaldoTotal('111.222.333-44');
+    DBMS_OUTPUT.PUT_LINE('Saldo Total: ' || v_saldo_total);
+END;
+/
 
 --CREATE OR REPLACE TRIGGER (COMANDO)
 -- Trigger para impedir que uma pessoa seja excluída se ela possuir apostas associadas
