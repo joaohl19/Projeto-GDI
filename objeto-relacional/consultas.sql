bcec9eebbe370c1ad4b4e5e44f1bfcf7bff1ab32
João Henrique Lessa <joaohenriquerlessa@gmail..com>
2024-03-06 23:37:55 -0300

    consultas

--- objeto-relacional/consultas.sql
+++ objeto-relacional/consultas.sql
@@ -88,14 +88,14 @@ order by ((P.valor * P.Odd) - P.valor);
 
 
 /*selecionar os indicadores, cujas pessoas indicadas possuem apenas telefones com ddd = 81 ou ddd = 83 e que fizeram apostas ao vivo, junto como o valor apostado, a odd e o número de bônus que cada um recebeu além do número de apostas ao vivo que cada um fez */
-select F.Nome, F.cpf, a.valor as valor_apostado, deref(a.apostas).Odd as odd, F.media_bonus, count(e.ID_Evento) as numero_de_apostas_ao_vivo
+select F.Nome, F.cpf, a.valor as valor_apostado, deref(a.apostas).Odd as odd, F.maior_bonus, count(e.ID_Evento) as numero_de_apostas_ao_vivo
 from tb_apostar a
 inner join (
-    select c.Saldo, J.Nome, J.cpf, J.media_bonus
+    select c.Saldo, J.Nome, J.cpf, J.maior_bonus
     from tb_pessoas_movimentam_contas h
     inner join tb_conta c on deref(h.Conta).ID_Conta = c.ID_Conta
     inner join (
-        select count(b.bonus_turbinado()) as media_bonus, deref(K.Indicador).Nome as Nome, deref(K.Indicador).cpf as cpf
+        select max(b.bonus_turbinado()) as maior_bonus, deref(K.Indicador).Nome as Nome, deref(K.Indicador).cpf as cpf
         from(
             select *
             from tb_pessoas l
@@ -111,8 +111,56 @@ inner join (
 ) F on deref(deref(a.pessoas_movimentam_contas).Pessoa).cpf = F.cpf
 inner join tb_evento_esportivo e on deref(a.evento_esportivo).ID_Evento = e.ID_Evento
 where deref(a.evento_esportivo).DataHora <= a.DataHora
-GROUP BY F.Nome, F.cpf, a.valor, deref(a.apostas).Odd, F.media_bonus
+GROUP BY F.Nome, F.cpf, a.valor, deref(a.apostas).Odd, F.maior_bonus
 order by F.cpf;
+
+
+/* seleciona a soma dos valores apostados*/
+select M.soma
+from( 
+    select sum(a.valor) as soma
+    from tb_apostar a
+    left join tb_resultado r on deref(a.apostas).ID_Aposta = r.ID_Aposta
+    left join tb_placar p on deref(a.apostas).ID_Aposta = p.ID_Aposta
+    left join tb_ambos_marcam amb on deref(a.apostas).ID_Aposta = amb.ID_Aposta
+    left join tb_gols g on deref(a.apostas).ID_Aposta = g.ID_Aposta  
+) M
+
+
+/* seleciona o valor total apostado em cada mercado a quantidade de apostas feitas em cada mercado*/
+select G.total_apostado_gols, G.apostas_gols, R.total_apostado_resultado, R.apostas_resultado, 
+       P.total_apostado_placar, P.apostas_placar, A.total_apostado_ambos, A.apostas_ambos
+from
+    (select sum(a.valor) as total_apostado_gols, count(g.ID_Aposta) as apostas_gols
+    from tb_apostar a
+    inner join tb_gols g on deref(a.apostas).ID_Aposta = g.ID_Aposta) G,
+    
+    (select sum(a.valor) as total_apostado_resultado, count(r.ID_Aposta) as apostas_resultado
+    from tb_apostar a
+    inner join tb_resultado r on deref(a.apostas).ID_Aposta = r.ID_Aposta) R,
+
+    (select sum(a.valor) as total_apostado_placar, count(p.ID_Aposta) as apostas_placar
+    from tb_apostar a
+    inner join tb_placar p on deref(a.apostas).ID_Aposta = p.ID_Aposta) P,
+
+    (select sum(a.valor) as total_apostado_ambos, count(amb.ID_Aposta) as apostas_ambos
+    from tb_apostar a
+    inner join tb_ambos_marcam amb on deref(a.apostas).ID_Aposta = amb.ID_Aposta) A 
+
+/* selecionar a nome, cpf, quantia movimentada e valor apostado pela pessoa que fez a aposta de maior valor*/
+
+select P.Nome, P.cpf, P.quantia_movimentada, P.valor_aposta
+from (
+       select a.valor as valor_aposta, deref(a.pessoas_movimentam_contas.Pessoa).Nome as Nome, 
+       deref(a.pessoas_movimentam_contas.Pessoa).cpf as cpf, 
+       deref(a.pessoas_movimentam_contas).valor as quantia_movimentada
+       from tb_apostar a
+) P
+order by P.valor_aposta desc
+fetch first 1 row only;
+
+
+
 
 
 
